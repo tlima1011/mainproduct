0762531ff50b82d63c7189e598eae15a3691ca56
tlima1011 <thiagolima1011@gmail.com>
2026-04-19 11:01:40 -0300

    Criacao de classes OrderItem e Product

---
+++ MainProduct/Entities/OrderItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace MainProduct.Entities
+{
+    internal class OrderItem
+    {
+        public int Quantity { get; set; }
+        public Product Product { get; set; } 
+
+        public OrderItem()
+        {
+
+        }
+        public OrderItem(int quantity, Product product)
+        {
+            Quantity = quantity;
+            Product = product; 
+        }
+
+        public double subTotal()
+        {
+            return Quantity * Product.Price; 
+        }
+
+        public override string ToString()
+        {
+            return "Product:\nName: " + Product.Name 
+                + "\nQuantity: " + Quantity 
+                + "\nUnity Price $" + Product.Price.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nSubtotal: $" +subTotal().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ MainProduct/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainProduct.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; } 
+
+        public Product()
+        {
+
+        }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
