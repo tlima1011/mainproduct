eb9ea2a54234cd2af244d4d2fcb803103cacb5cf
tlima1011 <thiagolima1011@gmail.com>
2026-04-19 09:52:59 -0300

    Criacao classe cliente

---
+++ MainProduct/Entities/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainProduct.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        Client()
+        {
+
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name +"\nEmail: " + Email + "\nBirth Date: " + BirthDate.ToString("dd/MM/yyyy");
+        }
+
+    }
+}
--- MainProduct/Program.cs
+++ MainProduct/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using MainProduct.Entities; 
+
 namespace MainProduct
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Informe o nome do cliente: ");
+            string name = Console.ReadLine();
+            Console.Write("E-mail: ");
+            string email = Console.ReadLine();
+            Console.Write("Informe data de nascimento (DD / MM / YYYY): "); 
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birthDate);
+
+            //Console.WriteLine(client);
+
         }
     }
 }
